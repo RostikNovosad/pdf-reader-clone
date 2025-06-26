792aca0ce3c047df30736008a9f615c9a5ef0e4a
Ростислав Новосад <rostyslavnovosad@MacBook-Pro-Rostislav.local>
2025-06-26 16:59:40 +0300

    comment fonts

--- index.ts
+++ index.ts
@@ -31,23 +31,25 @@ export function generatePdf(documentData: DocumentData, withDownload = false) {
     return {}
   })];
 
-  const fonts = {
-    NotoSans: {
-      normal: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-Regular.ttf',
-      // bold: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-Bold.ttf',
-      italics: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-Italic.ttf',
-      bolditalics: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-BoldItalic.ttf'
-    }
-  };
+  // const fonts = {
+  //   NotoSans: {
+  //     normal: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-Regular.ttf',
+  //     bold: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-Bold.ttf',
+  //     italics: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-Italic.ttf',
+  //     bolditalics: 'https://fonts-07cc13.gitlab.io/fonts/NotoSans-BoldItalic.ttf'
+  //   }
+  // };
   const docDefinition: any = {
     content,
     styles: { header: { fontSize: 18, bold: true, margin: [0, 0, 0, 10] } },
-    defaultStyle: {
-      font: 'NotoSans'
-    },
+    // defaultStyle: {
+    //   font: 'NotoSans'
+    // },
   };
   return new Promise((resolve, reject) => {
-    const pdf = pdfMake.createPdf(docDefinition, undefined, fonts);
+    // const pdf = pdfMake.createPdf(docDefinition, undefined, fonts);
+    const pdf = pdfMake.createPdf(docDefinition, undefined);
+
 
     if (withDownload) pdf.download('document.pdf');
 
